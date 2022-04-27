46e723c7b77763e31129411d739a62feef441a98
Aftab Mustafa <mustafa.aftab8906@yahoo.com>
2022-04-28 01:28:54 +0530

    Added new files

---
+++ Widgets/ProgressBar/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace ProgressBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver Driver = new ChromeDriver();
+
+            Driver.Manage().Window.Maximize();
+
+            Driver.Navigate().GoToUrl("https://demoqa.com/progress-bar");
+
+            Thread.Sleep(3000);
+
+            var ProgressControlBtn = Driver.FindElement(By.Id("startStopButton"));
+
+            ProgressControlBtn.Click();
+
+            Thread.Sleep(new System.Random().Next(3, 8) * 1000);
+
+            ProgressControlBtn.Click();
+
+            _ = 0;
+
+            Driver.Close();
+            Driver.Quit();
+        }
+    }
+}
---
+++ Widgets/ToolsTips/Program.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+
+namespace ToolsTips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Actions action = new Actions(Driver);
+
+            Driver.Manage().Window.Maximize();
+
+            Driver.Navigate().GoToUrl("https://demoqa.com/tool-tips");
+
+            Thread.Sleep(6000);
+
+            IWebElement HoverBtn = Driver.FindElement(By.Id("toolTipButton"));
+            action.MoveToElement(HoverBtn).Perform();
+            Thread.Sleep(2000);
+
+            IWebElement InputBoxHover = Driver.FindElement(By.Id("toolTipTextField"));
+            action.MoveToElement(InputBoxHover).Perform();
+            Thread.Sleep(2000);
+
+            IWebElement AnchorHover = Driver.FindElement(By.XPath("//a[text()='Contrary']"));
+            action.MoveToElement(AnchorHover).Perform();
+            Thread.Sleep(2000);
+
+            IWebElement DateHover = Driver.FindElement(By.XPath("//a[text()='1.10.32']"));
+            action.MoveToElement(DateHover).Perform();
+
+            _ = 0;
+
+            Driver.Close();
+            Driver.Quit();
+        }
+    }
+}
