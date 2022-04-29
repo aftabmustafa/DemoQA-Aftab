8a4684ac5350d4a2014126fd984edda8c5eaf572
Aftab Mustafa <mustafa.aftab8906@yahoo.com>
2022-04-29 22:23:48 +0530

    DQ Finish

--- Widgets/AutoCompete/Program.cs
+++ Widgets/AutoCompete/Program.cs
@@ -1,6 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
 using System.Threading;
 
 namespace AutoCompete
@@ -10,7 +9,6 @@ namespace AutoCompete
         static void Main(string[] args)
         {
             IWebDriver Driver = new ChromeDriver();
-            Actions action = new Actions(Driver);
 
             Driver.Manage().Window.Maximize();
             
@@ -18,10 +16,38 @@ namespace AutoCompete
 
             Thread.Sleep(3000);
 
+            TestMultiple(Driver);
 
+            TestSingle(Driver);
 
             Driver.Close();
             Driver.Quit();
+        }
+
+        static void TestMultiple(IWebDriver driver)
+        {
+            IWebElement InputBox = driver.FindElement(By.Id("autoCompleteMultipleInput"));
+
+            InputBox.SendKeys("ee");
+            Thread.Sleep(1000);
+            InputBox.SendKeys(Keys.Enter);
+
+            InputBox.SendKeys("Bl");
+            Thread.Sleep(500);
+
+            InputBox.SendKeys(Keys.ArrowDown);
+            Thread.Sleep(1000);
+
+            InputBox.SendKeys(Keys.Enter);
+        }
+
+        static void TestSingle(IWebDriver driver)
+        {
+            IWebElement InputBox = driver.FindElement(By.Id("autoCompleteSingleInput"));
+
+            InputBox.SendKeys("ee");
+            Thread.Sleep(1000);
+            InputBox.SendKeys(Keys.Enter);
         }
     }
 }
