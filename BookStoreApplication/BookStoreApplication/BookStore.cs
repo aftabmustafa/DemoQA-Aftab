43fc73ac19b2184eb32ff17068e2f48f451c3805
Aftab Mustafa <mustafa.aftab8906@yahoo.com>
2022-04-29 18:04:09 +0530

    Add Book Store

---
+++ BookStoreApplication/BookStoreApplication/BookStore.cs
@@ -0,0 +1,81 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace BookStoreApplication
+{
+    internal class BookStore
+    {
+        public void Home(IWebDriver driver)
+        {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://demoqa.com/books");
+
+            SearchBook(driver, "Git");
+            AddBook(driver);
+
+            SearchBook(driver, "script");
+            AddBook(driver);
+
+            NavigateProfile(driver);
+
+            DeleteBooks(driver);
+
+            Logout(driver);
+        }
+
+        private void SearchBook(IWebDriver driver, string BookName)
+        {
+            driver.FindElement(By.XPath("//input[@id='searchBox']")).SendKeys(BookName);
+            Thread.Sleep(1000);
+        }
+
+        private void AddBook(IWebDriver driver)
+        {
+            var books = driver.FindElements(By.XPath("//div[@class='rt-tr-group']//span//a"));
+            books[0].Click();
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.XPath("//button[text()='Add To Your Collection']")).Click();
+            Thread.Sleep(1000);
+
+            driver.SwitchTo().Alert().Accept();
+            driver.SwitchTo().DefaultContent();
+
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.XPath("//button[text()='Back To Book Store']")).Click();
+            Thread.Sleep(1000);
+        }
+
+        private void NavigateProfile(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://demoqa.com/profile");
+            Thread.Sleep(2000);
+        }
+
+        private void DeleteBooks(IWebDriver driver)
+        {
+            driver.FindElement(By.XPath("//div[contains(@class,'text-right')]//button[text()='Delete All Books']")).Click();
+
+            Thread.Sleep(1000);
+
+            driver.SwitchTo().ActiveElement();
+
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.Id("closeSmallModal-ok")).Click();
+
+            Thread.Sleep(1000);
+
+            driver.SwitchTo().Alert().Accept();
+        }
+
+        private void Logout(IWebDriver driver)
+        {
+            driver.FindElement(By.XPath("//div[contains(@class,'text-right')]//button[text()='Log out']")).Click();
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ BookStoreApplication/BookStoreApplication/Login.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace BookStoreApplication
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+            string username = "aria";
+            string password = "@riaLabel2022";
+
+            IWebDriver Driver = new ChromeDriver();
+
+            Driver.Manage().Window.Maximize();
+
+            Driver.Navigate().GoToUrl("https://demoqa.com/login");
+
+            Thread.Sleep(3000);
+
+            UserLogin(Driver, username, password);
+
+            RedirectBookStore(Driver);
+
+            Driver.Close();
+            Driver.Quit();
+        }
+
+        static void UserLogin(IWebDriver driver, string uName, string pass)
+        {
+            driver.FindElement(By.Id("userName")).SendKeys(uName);
+            driver.FindElement(By.Id("password")).SendKeys(pass);
+
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.Id("login")).Click();
+            Thread.Sleep(2500);
+        }
+
+        static void RedirectBookStore(IWebDriver driver)
+        {
+            new BookStore().Home(driver);
+        }
+    }
+}
